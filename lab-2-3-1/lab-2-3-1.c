0f10ed1b5d8870b947460a0a8c40f552895964c6
JiwonShin <dev.jiwonshin@gmail.com>
2022-04-11 21:08:30 +0900

    mac15

--- lab-2-3-1/lab-2-3-1.c
+++ lab-2-3-1/lab-2-3-1.c
@@ -9,7 +9,7 @@ const int Keypad[8] = {16, 13, 12, 6, 21, 26, 20, 19};
 // Variables will change:
 int ledState = HIGH;         // the current state of the output pin
 int buttonState;             // the current reading from the input pin
-int lastButtonState = HIGH;   // the previous reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
 
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
@@ -44,13 +44,11 @@ int KeypadRead() {
 
             if ((millis() - lastDebounceTime) > debounceDelay) {  //버턴변화가 50시간값 안에 발생했으면 바운스(채터링)으로 무시
 
-                if (reading != buttonState) { // 버턴이 On/Off 됐는지 확인(버턴의 변화가 일어났는가)
-                    buttonState = reading; //버턴의 변화가 일어났으면 그 변화를 기록했다가 다음 버턴 변화의 비교대상이 됨
+                buttonState = reading; //버턴의 변화가 일어났으면 그 변화를 기록했다가 다음 버턴 변화의 비교대상이 됨
 //                if (reading) {
-                    keypadnum = i;
-                    break;
+                keypadnum = i;
+                break;
 //                }
-                }
             }
         }
 
