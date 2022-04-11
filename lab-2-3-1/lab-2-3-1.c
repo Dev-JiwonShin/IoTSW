b96a735072efd58bf696ba6a5b3a52161a0c45c3
JiwonShin <dev.jiwonshin@gmail.com>
2022-04-11 20:47:11 +0900

    mac15

--- lab-2-3-1/lab-2-3-1.c
+++ lab-2-3-1/lab-2-3-1.c
@@ -6,6 +6,15 @@ const int LedRed[8] = {4, 17, 18, 27, 22, 23, 24, 25};
 const int Keypad[8] = {16, 13, 12, 6, 21, 26, 20, 19};
 // 3, 2, 1, 0, 7, 6, 5, 4
 
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
 
 //int debounce(int reading) {
 int debounce(int elem) {
@@ -50,22 +59,22 @@ int KeypadRead() {
 
 
         int reading = !digitalRead(Keypad[i]); //스위치 버턴상태값을 읽는다.
-        int debounceDelay = 50; //바운드에 대한 기준 시간값
-        unsigned long lastDebounceTime = 0; // 현재시간값을 가져와서 저장할 변수 자료형은 같은형으로 맞춤
-        int buttonstate = 1; //내부풀업모드 버턴이 초기값이 1이여서 초기값을 1로 잡음.
-        int lastButtonState = 1; //마지막 버턴상태를 기록변수
 
-        if (reading != lastButtonState) { //현재 읽은 버턴의 상태값과 이전상태가 다른지 체크
-            lastDebounceTime = millis(); // 다르면 변화(On/Off)가 일어난거니 그 시간을 변수에 저장시킨다.
-        }
 
-        if ((millis() - lastDebounceTime) > debounceDelay) {  //버턴변화가 50시간값 안에 발생했으면 바운스(채터링)으로 무시
+        if (reading) {
 
-            if (reading != buttonstate) { // 버턴이 On/Off 됐는지 확인(버턴의 변화가 일어났는가)
-                buttonstate = reading; //버턴의 변화가 일어났으면 그 변화를 기록했다가 다음 버턴 변화의 비교대상이 됨
-                if (reading) {
+            if (reading != lastButtonState) { //현재 읽은 버턴의 상태값과 이전상태가 다른지 체크
+                lastDebounceTime = millis(); // 다르면 변화(On/Off)가 일어난거니 그 시간을 변수에 저장시킨다.
+            }
+
+            if ((millis() - lastDebounceTime) > debounceDelay) {  //버턴변화가 50시간값 안에 발생했으면 바운스(채터링)으로 무시
+
+                if (reading != buttonstate) { // 버턴이 On/Off 됐는지 확인(버턴의 변화가 일어났는가)
+                    buttonstate = reading; //버턴의 변화가 일어났으면 그 변화를 기록했다가 다음 버턴 변화의 비교대상이 됨
+//                if (reading) {
                     keypadnum = i;
                     break;
+//                }
                 }
             }
         }
