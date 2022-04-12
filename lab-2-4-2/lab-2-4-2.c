c39a0a8fcc048f8fb7164e80ce73dd4295ac3911
JiwonShin <dev.jiwonshin@gmail.com>
2022-04-12 15:36:44 +0900

    mac17

--- lab-2-4-2/lab-2-4-2.c
+++ lab-2-4-2/lab-2-4-2.c
@@ -7,41 +7,8 @@ const int FndSelectPin[6] = { 4, 17, 18, 27, 22, 23 };
 // FND의 LED와 연결된 라즈베리파이 핀(A, B, …, H)
 const int FndPin[8] = { 6, 12, 13, 16, 19, 20, 26, 21 };
 // FND에 출력되는 문자 (0~9) 배열
-// const int FndFont[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67 };
-const int FndFont[5] = {0x76,0x79,0x38,0x38,0x3F};
-
-
-//return : 5 4 3 2 1 0 
-
-
-// 0 |	63	/077	0x3F	0 0 1 1   1 1 1 1
-// 1 |	6	/006	0x06	0 0 0 0   0 1 1 0
-// 2 |	91	/133	0x5B	0 1 0 1   1 0 1 1
-// 3 |	79	/117	0x4F	0 1 0 0   1 1 1 1
-// 4 |	102	/146	0x66	0 1 1 0   0 1 1 0
-// 5 |	109	/155	0x6D	0 1 1 0   1 1 0 1
-// 6 |	125	/175	0x7D	0 1 1 1   1 1 0 1
-// 7 |	7	/007	0x07	0 0 0 0   0 1 1 1
-// 8 |	127	/177	0x7F	0 1 1 1   1 1 1 1
-// 9 |	103	/147	0x67	0 1 1 0   0 1 1 1
-
-
-//							  G F E   D C B A
-// H |	118	/166	0x76	0 1 1 1   0 1 1 0
-// E |	121	/171	0x79	0 1 1 1   1 0 0 1
-// L |	56	/070	0x38	0 0 1 1   1 0 0 0
-// L |	56	/070	0x38	0 0 1 1   1 0 0 0
-// O |	63	/077	0x3F	0 0 1 1   1 1 1 1
-
-
-// _ H E L L O
-// H E L L O _
-// E L L O _ H
-// L L O _ H E
-// L O _ H E L
-// O _ H E L L
-// _ H E L L O
-
+const int FndFont[10] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66,
+	0x6D, 0x7D, 0x07, 0x7F, 0x67 };
 
 // 초기화 함수, WiringPi 라이브러리 초기화, Select 핀 및 LED 핀 초기화를 담당)
 void Init() {
@@ -60,7 +27,6 @@ void Init() {
 	}
 }
 
-
 // FND를 선택하는 함수, S0 ~ S5 중 파라미터(position)에 해당하는 FND 선택
 void FndSelect (int position) {
 	int i;
@@ -73,7 +39,6 @@ void FndSelect (int position) {
 		}
 	}
 }
-
 
 // FND를 출력하는 함수
 void FndDisplay(int position, int num) {
@@ -88,13 +53,9 @@ void FndDisplay(int position, int num) {
 	FndSelect( position ) ;
 }
 
-
 int main() {
 	int pos;
-	// int data[6] = { 5,4,3,2,1,0}; // 출력할 문자 데이터
-	// char data[7] = { " " ,"H","E","L","L","O"}; // 출력할 문자 데이터
-	// char data[7] = { "O","L","L","E","H"," "}; // 출력할 문자 데이터
-	char data[7] = " OLLEH"; // 출력할 문자 데이터
+	int data[6] = { 0, 1, 2, 3, 4, 5 }; // 출력할 문자 데이터
 	Init();
 	while(1) {
 		for( pos = 0; pos < 6; pos++ ) {
