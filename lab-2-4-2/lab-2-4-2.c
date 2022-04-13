75132855e1415b027a3151fcebf9b7b0ad997e43
JiwonShin <dev.jiwonshin@gmail.com>
2022-04-13 12:25:09 +0900

    mac18

--- lab-2-4-2/lab-2-4-2.c
+++ lab-2-4-2/lab-2-4-2.c
@@ -148,7 +148,7 @@ int main() {
             			// delay(1);
             			break;
             			default :
-            			delay(10);
+            			delay(1);
             			break;
             		}
             	}
