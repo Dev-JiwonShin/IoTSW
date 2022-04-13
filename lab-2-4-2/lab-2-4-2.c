8ca5e92587b8ef34d7d6061bea12a4236698c7cd
JiwonShin <dev.jiwonshin@gmail.com>
2022-04-13 12:42:13 +0900

    mac18

--- lab-2-4-2/lab-2-4-2.c
+++ lab-2-4-2/lab-2-4-2.c
@@ -32,7 +32,7 @@ unsigned long pre_time_2 = 0;
 unsigned long cur_time = 0;
 unsigned long cur_time_2 = 0;
 const int duration = 100;
-const long duration_2 = 100;
+const long duration_2 = 106;
 
 int digit_10e_2f = 0;
 int digit_10e_1f = 0;
@@ -120,23 +120,23 @@ int main() {
             		switch (pos) {
             			case 5 :
             			FndDisplay(pos, digit_10e3);
-            			// delay(1);
+            			delay(1);
             			break;
             			case 4:
             			FndDisplay(pos, digit_10e2);
-            			// delay(1);
+            			delay(1);
             			break;
             			case 3:
             			FndDisplay(pos, digit_10e1);
-            			// delay(1);
+            			delay(1);
             			break;
             			case 2:
             			FndDisplay_digit_10e0(pos, digit_10e0);
-            			// delay(1);
+            			delay(1);
             			break;
             			case 1:
             			FndDisplay(pos, digit_10e_1f);
-            			// delay(1);
+            			delay(1);
             			break;
             			case 0:
             			FndDisplay(pos, digit_10e_2f);
@@ -145,7 +145,7 @@ int main() {
             			}else{
             				digit_10e_2f=0;
             			}
-            			// delay(1);
+            			delay(1);
             			break;
             			default :
             			// delay(1);
